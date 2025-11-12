8440d29d2410d0f8b8aeb0cc21cd90b0af39c877
Traore Allassane <traoreallassane2255@gmail.com>
2025-11-12 08:33:40 +0000

    Setup + Configuration

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login/>} />
+        <Route path="/register" element={<Register/>} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
